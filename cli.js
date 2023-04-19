3eb32b27fa524bf5e4eef2a3b2ac7e5a69840c39
Praneeth Arrepu <parrepu@live.unc.edu>
2023-04-19 03:25:15 -0400

    the start of many commits

---
+++ cli.js
@@ -0,0 +1,10 @@
+#!/usr/bin/env node // Following should be the first line of the script file
+
+// Good practice to include all needed imports or dependencies at the top of the file.
+// import moment from moment-timezone; Might need for later.
+import minimist from "minimist";
+import fetch from "node-fetch";
+import moment from "moment-timezone";
+
+// const timezone = moment.tz.guess(); Will likely need for later.
+
